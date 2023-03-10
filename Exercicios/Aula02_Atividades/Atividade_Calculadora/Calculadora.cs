80524da82e6d9d9f7e9bf51e149e18d89d36cb4d
hernrique-roldao <henriquerolda@rede.ulbra.br>
2023-03-09 23:43:32 -0300

    Atividade Calculadora

---
+++ Exercicios/Aula02_Atividades/Atividade_Calculadora/Calculadora.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Atividade_Calculadora
+{
+    public class Calculadora
+    {
+        public double Somar(double num1, double num2) {
+            return num1 + num2;
+        }
+
+        public double Subtrair(double num1, double num2) {
+            return num1 - num2;
+        }
+    }
+}
---
+++ Exercicios/Aula02_Atividades/Atividade_Calculadora/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Crie uma classe chamada "Calculadora" que tenha dois métodos: "Somar" e "Subtrair". 
+O método "Somar" deve receber dois números como parâmetros e retornar a soma desses números. 
+O método "Subtrair" deve receber dois números como parâmetros e retornar a subtração desses números. 
+Crie um objeto dessa classe e utilize os métodos para realizar operações matemáticas.*/
+
+using Atividade_Calculadora;
+
+Calculadora calculadora = new Calculadora();
+Console.WriteLine(calculadora.Somar(2, 4));
+Console.WriteLine(calculadora.Subtrair(6, 2));
